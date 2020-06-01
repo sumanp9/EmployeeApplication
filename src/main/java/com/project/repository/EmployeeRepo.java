cfcab93dfffbfaaee6cf85f22e07e625e04fb0cf
sumanp9 <11.sumanpradhan@gmail.com>
2020-06-01 11:32:43 -0500

    Created Employee Repository Interface

---
+++ src/main/java/com/project/repository/EmployeeRepo.java
@@ -0,0 +1,9 @@
+package com.project.repository;
+
+import com.project.pojomodel.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+}
