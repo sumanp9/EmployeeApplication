f44bf245de8671b03ae0699fb32c1c5dab3255cd
sumanp9 <11.sumanpradhan@gmail.com>
2020-06-01 10:53:32 -0500

    1. Added devtools and mysql dependencies. 2.Renamed main application java file

--- src/main/java/com/project/emp/EmployeeDetails/Application.java
+++ src/main/java/com/project/emp/EmployeeDetails/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmployeeDetailsApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeeDetailsApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
